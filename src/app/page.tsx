72374b9ab25f48713603db5f24567648f037914d
waleedfadl <wwaleedfadl@gmail.com>
2025-05-20 18:57:26 +0300

    adding convex as a backend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,7 @@
-import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-
 export default function Home() {
   return (
     <div className="m-10">
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+      Home
     </div>
 
   );
