3a6ac5b978a333e231a561f459cf4254177e337d
waleed fadl <wwwaleedfadl@gmail.com>
2025-05-25 17:19:33 +0300

    adding login func

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div className="m-10">
-      Home
+      <SignInButton><Button>Login</Button></SignInButton>
     </div>
   );
 }
