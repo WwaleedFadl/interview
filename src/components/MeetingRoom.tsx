bcef36608554d7f6ebf543ca081f15d3269cd55b
waleed fadl <wwwaleedfadl@gmail.com>
2025-06-10 21:17:44 +0300

    adding resizables

--- src/components/MeetingRoom.tsx
+++ src/components/MeetingRoom.tsx
@@ -1,6 +1,36 @@
+import { useCallStateHooks, CallingState } from "@stream-io/video-react-sdk"
+import { Loader } from "lucide-react"
+import { useRouter } from "next/navigation"
+import { useState } from "react"
+import { ResizablePanel, ResizablePanelGroup } from "./ui/resizable"
+
 const MeetingRoom = () => {
+  const router = useRouter()
+  const [layout, setLayout] = useState<"grid" | "speaker">('speaker')
+  const [showParticipants, setShowParticipants] = useState(false)
+  const { useCallCallingState } = useCallStateHooks()
+  const callingState = useCallCallingState()
+  if (callingState !== CallingState.JOINED) {
+    return (
+      <div className="h-96 flex items-center justify-center">
+        <Loader className="size-6 animate-spin" />
+      </div>
+    )
+  }
+
+
   return (
-    <div></div>
+    <ResizablePanelGroup direction="horizontal">
+      <ResizablePanel defaultSize={35} minSize={25} maxSize={100} className="relative">
+        <h1>Video will go here</h1>
+      </ResizablePanel>
+
+      <ResizablePanel defaultSize={65} minSize={25}>
+        <h1>Code Editor Will Go Here</h1>
+
+      </ResizablePanel>
+
+    </ResizablePanelGroup>
   )
 }
 
