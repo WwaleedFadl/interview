843d624d166b3037d41c361c9ee3564989badda3
waleed fadl <wwwaleedfadl@gmail.com>
2025-06-14 22:01:00 +0300

    adding schedule page

--- src/app/(root)/schedule/page.tsx
+++ src/app/(root)/schedule/page.tsx
@@ -1,4 +1,15 @@
+'use client'
+
+import LoaderUI from "@/components/LaderUI";
+import { useUserRole } from "@/hooks/useUserRole";
+import { useRouter } from "next/navigation";
+import InterviewScheduleUI from "./InterviewScheduleUI";
+
 const SchedulePage = () => {
-	return <div>Schedule Page</div>;
+  const router = useRouter()
+  const { isInterviewer, isLoading } = useUserRole()
+  if (isLoading) return <LoaderUI />
+  if (!isInterviewer) return router.push('/')
+  return <InterviewScheduleUI />;
 };
 export default SchedulePage;
